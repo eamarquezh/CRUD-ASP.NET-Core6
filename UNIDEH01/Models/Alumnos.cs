e8dee374d3c155851bb3418cd218c3d0ef459acf
eamarquezh <supericman@outlook.com>
2022-04-02 02:24:37 -0600

    actualizacion de validacion de campos

--- UNIDEH01/Models/Alumnos.cs
+++ UNIDEH01/Models/Alumnos.cs
@@ -5,25 +5,46 @@ namespace UNIDEH01.Models
 	{
 		public int Id { get; set; }
 
+		[Display(Name = "Nombre")]
+		[Required(ErrorMessage = "{0} requerido")]
+		[RegularExpression(@"^[ \s{A-Z}{a-z}ñÑ]{4,50}$", ErrorMessage = "{0} incorrecto")]
 		public string? nombres { get; set; }
 
+		[Display(Name = "Apellidos")]
+		[Required(ErrorMessage = "{0} requerido")]
+		[RegularExpression(@"^[ \s{A-Z}{a-z}ñÑ]{4,50}$", ErrorMessage = "{0} incorrecto")]
 		public string? apellidos { get; set; }
 
-		[Display(Name = "Release Date")]
+		[Display(Name = "Fecha de nacimiento")]
 		[DataType(DataType.Date)]
 		public DateTime fecha_de_nacimiento { get; set; }
 
+		[Display(Name = "Grado")]
+		[Required(ErrorMessage = "{0} requerido")]
 		public int grado { get; set; }
 
+		[Display(Name = "Sección")]
+		[RegularExpression(@"^[ \s{A-Z}{a-z}{0-9}ñÑó]{4,50}$", ErrorMessage = "{0} incorrecto")]
+		[Required(ErrorMessage = "{0} requerido")]
 		public string? seccion { get; set; }
 
+		[Display(Name = "Dirección")]
+		[RegularExpression(@"^[ \s{A-Z}{a-z}{0-9}ñÑó#.-]{4,50}$", ErrorMessage = "{0} incorrecto")]
+		[Required(ErrorMessage = "{0} requerido")]
 		public string? direccion { get; set; }
 
+		[Display(Name = "Telefono")]
+		[Required(ErrorMessage = "{0} requerido")]
+		[RegularExpression(@"^[ \s{0-9}]{10}$", ErrorMessage = "{0} incorrecto")]
 		public string? telefono { get; set; }
 
-		[DataType(DataType.EmailAddress)]
+		[Display(Name = "Correo electronico")]
+		[Required(ErrorMessage = "{0} requerido")]
+		[RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "{0} incorrecto")]
 		public string? correo { get; set; }
 
+		[Display(Name = "Contraseña")]
+		[Required(ErrorMessage = "{0} requerido")]
 		public string? pass { get; set; }
 	}
 }
--- UNIDEH01/Models/Notas.cs
+++ UNIDEH01/Models/Notas.cs
@@ -7,14 +7,19 @@ namespace UNIDEH01.Models
 	{
 		public int Id { get; set; }
 
+		[Display(Name = "Id alumno")]
 		public int idalumnos { get; set; }
 
+		[Display(Name = "Id Curso")]
 		public int idcurso { get; set; }
 
+		[Display(Name = "Sección")]
+		[RegularExpression(@"^[ \s{A-Z}{a-z}{0-9}ñÑó]{4,50}$", ErrorMessage = "{0} incorrecto")]
+		[Required(ErrorMessage = "{0} requerido")]
 		public string? unidad { get; set; }
 
-		[Column(TypeName = "decimal(2, 2)")]
-		public decimal promedio { get; set; }
+		[Display(Name = "Promedio")]
+		public int promedio { get; set; }
 	}
 
 }
